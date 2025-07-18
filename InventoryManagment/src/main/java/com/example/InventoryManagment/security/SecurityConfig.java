dea4a171c520eda7f8eb9dcdae0889102758182e
hello_khoa <118266027+Khoacoder22@users.noreply.github.com>
2025-07-18 11:20:11 +0700

    Adding security config

--- InventoryManagment/src/main/java/com/example/InventoryManagment/security/SecurityConfig.java
+++ InventoryManagment/src/main/java/com/example/InventoryManagment/security/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.example.InventoryManagment.security;
 
+import com.example.InventoryManagment.exception.CustomAccessDenialHandler;
+import com.example.InventoryManagment.exception.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +23,26 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Slf4j
 public class SecurityConfig {
     private final AuthFilter authFilter;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final CustomAccessDenialHandler customAccessDenialHandler;
+
+    //    AuthFilter Bộ lọc JWT của bạn – kiểm tra token từ request và xác thực người dùng.
+    // CustomAuthenticationEntryPoint Xử lý khi người dùng chưa đăng nhập mà đòi truy cập (401 Unauthorized).
+    // CustomAccessDenialHandler	Xử lý khi người dùng không đủ quyền (403 Forbidden).
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .exceptionHandling(exceptionHandling -> exceptionHandling.accessDeniedHandler(customAccessDenialHandler)
+                        .authenticationEntryPoint(customAuthenticationEntryPoint)
+                )
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
 
 }
